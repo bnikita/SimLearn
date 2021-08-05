76b2954ebab29ddb0fdb8df5852247a9fb41bca0
bnikita <bnikita.itmo@gmail.com>
2021-08-05 13:57:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 st.text('Загрузка las файла/файлов')
 st.button('LAS')
+st.text('')
 st.text ('Диапазон глубины в скважине(ах) для определения похожести')
-st.slider('Depth')
+st.slider('Smart depth')
+st.slider('End depth')
+st.text('')
+st.SelectBox('Выбор предобученной модели (в зависимости от вида каротажей)',('RNN on 4 logs', 'RNN on 6 logs'))
